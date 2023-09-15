81376702fb2a285db3ff3ca729ddabb89e4a5777
isbakhann <isbahkhann@gmail.com>
2023-09-15 15:44:32 +0500

    two file added

---
+++ app.js
@@ -0,0 +1,90 @@
+//q1
+// var value = ["ahmer", "Salar", "imama"];
+
+// q3
+// var val = ["ahmed", "Salar", "imama"];
+
+// q4
+// var num = [6, 24, 26];
+
+// q5
+// var boolean = [true, false];
+
+// q6
+// var arr1 = ["Imama", "Salar", 6, true, 24];
+
+// q7
+// var degree =
+//     ["SSC", "HSC", "BCS",
+//         "BS", "BCOM", "MS", "M.Phill", "PhD"];
+// console.log(degree);
+
+// q8
+// var students = ["Imama", "Salar", "abcd"];
+// var score = [320, 230, 480];
+
+// var totalMarks = 500;
+
+// var percentage = (score[0] / totalMarks) * 100;
+// var percentage1 = (score[1] / totalMarks) * 100;
+// var percentage2 = (score[2] / totalMarks) * 100;
+
+
+// console.log("socre of" + " " + students[0] + " " + "is" + " " + score[0] + " " + "percentage:" + " " + percentage + "%");
+// console.log("socre of" + " " + students[1] + " " + "is" + " " + score[1] + " " + "percentage:" + " " + percentage1 + "%");
+// console.log("socre of" + " " + students[2] + " " + "is" + " " + score[2] + " " + "percentage:" + " " + percentage2.toFixed(1) + "%");
+
+// q9a
+// var colours = ["red", "black", "purple", "pink", "maroon"];
+// var user = prompt("enter colour");
+// colours.unshift(user);
+// console.log(colours);
+
+// q9b
+// var colours = ["red", "black", "purple", "pink", "green"];
+// var user = prompt("enter colour");
+// colours.push(user);
+// console.log(colours);
+
+// q9c
+// var colours = ["red", "black", "purple", "pink", "green"];
+// colours.unshift("voilent", "indigo");
+// console.log(colours);
+
+// q9d
+// var colours = ["red", "black", "purple", "pink", "green"];
+// colours.shift();
+// console.log(colours);
+
+// q9e
+// var colours = ["red", "black", "purple", "pink", "green"];
+// colours.pop();
+// console.log(colours);
+
+
+// q9f
+// var colours = ["red", "black", "purple", "pink", "green"];
+// var user = +prompt("enter desired position ");
+// var user1 = prompt("enter colour");
+// colours.splice(user,0, user1);
+// console.log(colours);
+
+// q9g
+// var colours = ["red", "black", "purple", "pink", "green"];
+// var user = +prompt("enter desired position ");
+// var user1 = prompt("enter colour number");
+// colours.splice(user,user1);
+// console.log(colours);
+
+// q10
+
+//Q11
+// var city=["Karachi","lahor","Islamabad","quetta","Peshawar"];
+// console.log(city);
+// var copy=city.slice(2,4);
+// console.log(copy);
+//12
+// var arr = ["This", "is", "my", "cat"];
+// var convert=arr.join();
+// console.log(convert);
+// var arr1 = ["This" ,"is" ,"my" ,"cat"]s
